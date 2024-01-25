1af683624e15a9e8e3a13797d2202e2602658e1b
Dlcnokc <Dlcnokc@outlook.com>
2024-01-24 23:46:47 -0600

    Added AG Project
    Added AG Project for live view.

---
+++ AG-Website01/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("menuIcon").style.opacity = "0";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("menuIcon").style.opacity = "1";
+  }
+
+  
